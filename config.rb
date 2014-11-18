fce7bd7ac15361c1e9bd250af7eaa237c93537d9
Ryan MacInnes <ryan.macinnes@gmail.com>
2014-11-17 16:38:01 -0800

    Adding font awesome.

--- config.rb
+++ config.rb
@@ -55,6 +55,7 @@ require 'slim'
 set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
+set :fonts_dir, 'fonts'
 set :api_root, 'http://localhost:4000/api/v1'
 
 # Build-specific configuration
